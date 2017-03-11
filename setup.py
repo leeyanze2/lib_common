25dd1dbc9c205eba497deab8f9790d1257fee915
Malcolm Lee <leeyanzesg@yahoo.com.sg>
2017-03-11 22:09:26 +0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='lib_common',
+    version='1.0.0',
+    url='https://github.com/leeyanze2/lib_common.git',
+    author='Malcolm Lee',
+    # author_email='?',
+    # description='?',
+)
