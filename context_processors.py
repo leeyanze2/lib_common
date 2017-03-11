9e54b869f392b7131f0a463f9341c0da544bc5a7
Malcolm Lee <leeyanzesg@yahoo.com.sg>
2017-03-11 23:01:16 +0800

    Editing lib_common submodule from slipper shop

--- context_processors.py
+++ context_processors.py
@@ -1,6 +1,5 @@
 def base_html(request):
     from generic_ecom.settings import NV_CLIENT_DETAILS
-    print "shoe update lib_common testing"
     return {
         'nv_client': {
             "shop_name": NV_CLIENT_DETAILS.get("SHOP_NAME", "")
